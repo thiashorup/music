4f2ca5ebf073a6a7bdfe18eadacaa162d60f6788
thiashorup <69422583+thiashorup@users.noreply.github.com>
2022-09-22 16:26:10 +0200

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -1,12 +1,12 @@
 module.exports = {
-  Admins: ["668818456673779737"], //Admins of the bot
-  ExpressServer: true,//If you wanted to make the website run or not
+  Admins: ["1012271649871122503"], //Admins of the bot
+  ExpressServer: false,//If you wanted to make the website run or not
   DefaultPrefix: process.env.Prefix || "-", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/SchEbTFaNM", //Donot change..
-  Token: process.env.Token || "Your_Token_Here", //Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "Your_Client_Id_Here", //Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "Your_Client_Secret_Here", //Discord Client Secret
+  Token: process.env.Token || "MTAyMjUwNjE2ODYyMDQyOTM0Mg.GrFxlf.hcOyoa0-cucPDbPaEzH7HXM4XbcKUPdmP43GSc", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "1022506168620429342", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "xUiZDWWuhZM9n8UXfKGE4kw02yhYAEv2", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   "24/7": false, //If you want the bot to be stay in the vc 24/7
